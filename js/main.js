f3abfb02460693e0f66ca551fa343fe52cd1bbf3
shalahinova <kat.shalaginova@gmail.com>
2018-04-22 17:53:16 +0300

    Added castom settings for carousel in main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+    $("#sliderContaier").owlCarousel(
+        {
+            loop: true,
+            margin: 10,
+            nav: true,
+            navText: [' ', ' '],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
+            }
+    });
 });
